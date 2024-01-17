24d4b2b6eea4e8532a96ed53324ccc01d994b6bc
WixdomCoding <106668839+WixdomCoding@users.noreply.github.com>
2024-01-17 09:42:15 +0100

    Almost complete
    Just need to finish descriptions of the projects

--- src/App.js
+++ src/App.js
@@ -2,30 +2,29 @@ import React, { useState } from 'react';
 import './App.css';
 import './Animations.css';
 
-function App() {
   const slides = [
     {
       imgSrc: './portfolio.png',
       projectName: 'My portfolio',
-      projectDescription: 'This project is my portfolio, it is made with React and is the current website that you are currently on.',
+      projectDescription: 'This project is my portfolio, it is made with React and is the current website that you are on. I had a limited time to make it, so the project has not reached its full potential yet.',
       githubLink: 'https://github.com/WixdomCoding/portfolio',
     },
     {
       imgSrc: './prisma.png',
       projectName: 'Prisma tenta',
-      projectDescription: 'Jag utvecklade en interaktiv webbplats där användare kan registrera sig med ett användarnamn, lösenord och välja en roll (antingen som admin eller användare). All användardata lagras i en MySQL-databas. Webbplatsen kräver inloggning, och beroende på användarens roll kommer hen antingen att få möjlighet att skapa och publicera blogginlägg eller endast att läsa dessa inlägg. För att uppnå detta använde jag HTML, CSS, Node.js, Express, Prisma och MySQL. (Bilden var tagen när webbsidan inte var kopplad till databasen, därför är alla values fel).',
+      projectDescription: "I developed an interactive website where users can register with a username, password, and choose a role (either as an admin or a regular user). All user data is stored in a MySQL database. The website requires login, and depending on the user's role, they will either have the ability to create and publish blog posts or only read these posts. To achieve this, I used HTML, CSS, Node.js, Express, Prisma, and MySQL. (The image was taken when the website was not connected to the database, hence all values are incorrect).",
       githubLink: 'https://github.com/WixdomCoding/Daniel_tenta3',
     },
     {
       imgSrc: './nodecrud.png',
       projectName: 'NodeJS CRUD',
-      projectDescription: 'NodeJS CRUD Description',
+      projectDescription: 'A simple CRUD application made with NodeJS. ',
       githubLink: 'https://github.com/WixdomCoding/NodeJS-CRUD',
     },
     {
       imgSrc: './dgdlyrics.png',
       projectName: 'DGD Lyrics Reborn',
-      projectDescription: 'A website hosting lyrics for the band Dance Gavin Dance.',
+      projectDescription: 'A website hosting lyrics for the band Dance Gavin Dance. ',
       githubLink: 'https://github.com/WixdomCoding/DGD-Lyrics-Reborn',
     },
     {
@@ -74,6 +73,7 @@ function App() {
 
   ];
 
+const App = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const goToNextSlide = () => {
@@ -83,99 +83,132 @@ function App() {
   const goToPrevSlide = () => {
     setCurrentSlide((prevIndex) => (prevIndex - 1 + slides.length) % slides.length);
   };
-
+  
   return (
     <div>
-      <div id='page1'>
+      <section id='page1'>
         <div id='topholder'>
           <header id='firstfloat'>
             <div id='firstfloattext'>
               <h1 id='welcome'>Welcome</h1>
               <h2 id='introduction'>
-                My name is 
-                <span class='namecolor'> Daniel Parraguez Öberg</span>
+                My name is <span className='namecolorgold'>Daniel Parraguez Öberg</span>
               </h2>
-              <h3 id='description'>I am a full stack developer, but i prefer frontend development. <br /><br />The current coding languages i know are the following:</h3>
+              <h3 id='description'>
+                I am a full stack developer, but I prefer frontend development.
+                <br />
+                <br />
+                The current coding languages I know are
+              </h3>
               <div id='languageholder'>
-                <img id='htmllang' src='./languages/html.png'></img>
-                <img id='csslang' src='./languages/css.png'></img>
-                <img id='jslang' src='./languages/javascript.png'></img>
-                <img id='nodelang' src='./languages/node.png'></img>
-                <img id='reactlang' src='./languages/react.png'></img>
-                <img id='phplang' src='./languages/php.png'></img>
-
+                <a href='https://en.wikipedia.org/wiki/HTML5'><img id='htmllang' src='./languages/html.png' alt='HTML'></img></a>
+                <a href='https://en.wikipedia.org/wiki/CSS'><img id='csslang' src='./languages/css.png' alt='CSS'></img></a>
+                <a href='https://en.wikipedia.org/wiki/JavaScript'><img id='jslang' src='./languages/javascript.png' alt='JavaScript'></img></a>
+                <a href='https://en.wikipedia.org/wiki/Node.js'><img id='nodelang' src='./languages/node.png' alt='Node.js'></img></a>
+                <a href='https://en.wikipedia.org/wiki/React_(software)'><img id='reactlang' src='./languages/react.png' alt='React'></img></a>
+                <a href='https://en.wikipedia.org/wiki/PHP'><img id='phplang' src='./languages/php.png' alt='PHP'></img></a>
               </div>
             </div>
-
           </header>
-          
+  
           <div id='secondfloat'>
             <div className='box'>
               <div id='metxtholder'>
-                <p id='metxt'>So what makes <br />
-                <span class='namecolor'>me</span>, <span class='namecolor'>me</span>?</p>
-                <img id='vector' src='Vector.png'></img>
+                <p className='zoom' id='metxt'>
+                  <span id='so'>So </span>
+                  <span id='what'>what </span>
+                  <span id='makes'>makes </span>
+                  <span className='namecolorgold me1'>me</span>,{' '}
+                  <span className='namecolorgold me2'>me</span>?
+                </p>
+                <img id='vector' src='Vector.png' alt='Vector'></img>
               </div>
-
             </div>
-
           </div>
         </div>
-      </div>
-
-      <div id='page2'>
+      </section>
+  
+      <section id='page2'>
         <header>
           <h1 id='welcome2'>About me</h1>
           <p id='abouttext'>
-            Hello! I am a Swedish / Chilean studying fullstack developer currently living in Stockholm, Sweden. <br />
-            Currently, I am studying at NTI Gymnasiet Södertörn in the TE4 class.
-            I have been coding for about 3 and a half years now, and I have learned a lot during that time.
-            I have learned a lot of different languages including some that are not used in web development, but I prefer frontend development.
-            React is one of the more recent languages that I have learnt and I have been using it for a couple months now.
+            Hello! I am a Swedish/Chilean studying fullstack developer currently living in Stockholm, Sweden.
+            <br />
+            Currently, I am studying at NTI Gymnasiet Södertörn in the TE4 class. I have been coding for about 3 and a half years now, and I have learned a lot during that time. I have learned a lot of different languages, including some that are not used in web development, but I prefer frontend development. React is one of the more recent languages that I have learnt and I have been using it for a couple of months now.
           </p>
         </header>
         <div>
-          <img id='me' src='daniel.png'></img>
+          <img id='me' className='zoom' src='daniel.png' alt='Daniel'></img>
         </div>
-      </div>
-
-      <div id='page3'>
+      </section>
+  
+      <section id='page3'>
         <header>
           <h1 id='welcome3'>Projects</h1>
         </header>
-          
-            <div id='projectsholder'>
-                <div id='floaterslideshow'>
-
-                  <div id='buttonleft'>
-                    <button className='arrow-button' onClick={goToPrevSlide}>{'<'}</button>
-                  </div>
-                  <div>
-
-                    <div>
-
-                      <img className='projectimg' src={slides[currentSlide].imgSrc} alt='Project Image'></img>
-                      <h2 className='projectname'>{slides[currentSlide].projectName}</h2>
-
-                    </div>
-
-                  </div>
-
-                  <div id='buttonright'>
-                  <button className='arrow-button' onClick={goToNextSlide}>{'>'}</button>
-                  </div>
-
+        <div>
+          <div id='projectsholder'>
+            <div id='floaterslideshow'>
+              <div id='buttonleft'>
+                <button className='arrow-button zoom' onClick={goToPrevSlide}>{'<'}</button>
               </div>
-
+              <div>
+                <div>
+                  <img className='projectimg' src={slides[currentSlide].imgSrc} alt='Project'></img>
+                </div>
+              </div>
+              <div id='buttonright'>
+                <button className='arrow-button zoom' onClick={goToNextSlide}>{'>'}</button>
+              </div>
             </div>
-            <div id='projdescholder'>
+          </div>
+          <div id='projdescholder'>
             <div id='anchorholder'>
-              <a href={slides[currentSlide].githubLink} target='_blank'>GitHub Link</a>
+             <h2 className='projectname'>{slides[currentSlide].projectName}</h2>
+              <a href={slides[currentSlide].githubLink} target='_blank' rel='noopener noreferrer'>
+                GitHub Link
+              </a>
             </div>
             <p className='projectdescription'>{slides[currentSlide].projectDescription}</p>
+          </div>
+        </div>
+      </section>
+  
+      <footer>
+        <h1 id='footername'>Daniel Parraguez Öberg</h1>
+        <div id='footerholder'>
+          <div id='footerleft'>
+            <div id='contactme'>
+              <div>
+                <b>Contact me</b>
+              </div>
+              <div>
+                <p>079-335 25 90</p>
+              </div>
+              <div>
+                <p>business.danielparraguezoberg@gmail.com</p>
+              </div>
             </div>
-
-      </div>
+          </div>
+          <div id='footerright'>
+            <div id='findme'>
+              <b>Find me here</b>
+            </div>
+            <div>
+              <a href='https://github.com/WixdomCoding' target='_blank' rel='noopener noreferrer'>
+                <img className='footericon zoom' src='./githubicon.png' alt='GitHub'></img>
+              </a>
+              <a
+                href='https://www.linkedin.com/in/daniel-parraguez-%C3%B6berg-29753b2a5/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <img className='footericon zoom' src='./linkedinicon.png' alt='LinkedIn'></img>
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
